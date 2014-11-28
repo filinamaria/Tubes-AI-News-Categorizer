088c406d2eec6b2ee7b38bb61afe7ecf1323e34a
MarioFilino <mario.filino@gmail.com>
2014-11-28 14:35:42 +0700

    add getConnection method to class NewsClassifier

--- src/java/weka/classifier/NewsClassifier.java
+++ src/java/weka/classifier/NewsClassifier.java
@@ -6,11 +6,16 @@
 
 package weka.classifier;
 
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import weka.classifiers.Evaluation;
+import javax.mail.internet.NewsAddress;
 import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayesMultinomial;
 import weka.core.Instances;
 import weka.experiment.InstanceQuery;
@@ -32,14 +37,53 @@ public class NewsClassifier {
         
     }
     
-    private Instances getData() throws Exception{
-        InstanceQuery query = new InstanceQuery();
+    public static void main(String args []){
+        try {
+            NewsClassifier lalala = new NewsClassifier();
+            Instances data = lalala.getData();
+            if(data != null){
+                out.println("bisa");
+            }else{
+                out.println("tidak bisa");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(NewsClassifier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private Connection getConnection() throws ClassNotFoundException, SQLException, IllegalAccessException{
+        Connection connection = null;
         
-        query.setDatabaseURL(DB_URL);
-        query.setUsername(Username);
-        query.setPassword(Password);
-        query.setQuery("select FULL_TEXT, LABEL from artikel natural join artikel_kategori_verified natural join kategori");
-        Instances data = query.retrieveInstances();
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            String url = DB_URL;
+            String user = Username;
+            String password = Password;
+            connection =  DriverManager.getConnection(url, user, password);
+        }catch(ClassNotFoundException | InstantiationException e){
+            e.printStackTrace();
+        }
+        
+        return connection;
+    }
+    
+    private Instances getData() throws Exception{
+        Instances data = null;
+        
+        try{
+            Connection connection = getConnection();
+            InstanceQuery query = new InstanceQuery();
+        
+            query.setDatabaseURL(DB_URL);
+            query.setUsername(Username);
+            query.setPassword(Password);
+            query.setQuery("select FULL_TEXT, LABEL from artikel natural join artikel_kategori_verified natural join kategori");
+            data = query.retrieveInstances();
+            
+            connection.close();
+        }catch(SQLException e){
+            
+        }
         
         return data;
     }
@@ -64,16 +108,7 @@ public class NewsClassifier {
 		cls.buildClassifier(data);
 		return cls;
 	}
-	
-	public String doClassify(String text) {
-		try {
-			Instances trainingSet = getData();
-			Instances toClassify = 
-		} catch (Exception ex) {
-			Logger.getLogger(NewsClassifier.class.getName()).log(Level.SEVERE, null, ex);
-		}
-	}
-	
+		
 	public void evaluateClassifier(Classifier cls, Instances data) throws Exception{
 		Evaluation eval = new Evaluation(data);
 		eval.crossValidateModel(cls, data, 10, new Random(1));     
@@ -102,11 +137,13 @@ public class NewsClassifier {
 		trainingSet.setClass(trainingSet.attribute(namaKelas));
 		Classifier cls = makeClassifier(trainingSet);
 		
-		double clsLabel = cls.classifyInstance(unlabeled.instance(i));
-		labeled.instance(i).setClassValue(clsLabel);
-		}
+		//double clsLabel = cls.classifyInstance(unlabeled.instance(i));
+		//labeled.instance(i).setClassValue(clsLabel);
+		//}
 		
-		return labeled;
+		//return labeled;
+                return null;
 	}
+        
 	
 }
