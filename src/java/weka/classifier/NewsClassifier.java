6719542ccda01addef9f19894e30b6b7f2487e78
Anggi <cilvia.sianora@gmail.com>
2014-11-28 12:48:04 +0700

    add classify weka

--- src/java/weka/classifier/NewsClassifier.java
+++ src/java/weka/classifier/NewsClassifier.java
@@ -11,12 +11,55 @@ import weka.classifiers.Evaluation;
 import weka.classifiers.Classifier;
 import weka.classifiers.bayes.NaiveBayesMultinomial;
 import weka.core.Instances;
+import weka.experiment.InstanceQuery;
+import weka.filters.*;
+import weka.filters.unsupervised.attribute.NominalToString;
+import weka.filters.unsupervised.attribute.NumericToNominal;
+import weka.filters.unsupervised.attribute.StringToWordVector;
 
 /**
  *
  * @author Sakurai
  */
 public class NewsClassifier {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/news_aggregator";
+    private static final String Username = "root";
+    private static final String Password = "";
+    
+    public NewsClassifier(){
+        
+    }
+    
+    private Instances getData() throws Exception{
+        InstanceQuery query = new InstanceQuery();
+        
+        query.setDatabaseURL(DB_URL);
+        query.setUsername(Username);
+        query.setPassword(Password);
+        query.setQuery("select JUDUL, FULL_TEXT, ID_KELAS from artikel natural join artikel_kategori_verified");
+        Instances data = query.retrieveInstances();
+        
+        return data;
+    }
+    
+    private Instances doFilter(Instances data) throws Exception{
+        NumericToNominal numericToNominal = new NumericToNominal();
+        numericToNominal.setAttributeIndices("last");
+        
+        NominalToString nominalToString = new NominalToString();
+        nominalToString.setAttributeIndexes("1,2");
+        
+        StringToWordVector stringToWordVector = new StringToWordVector();
+        stringToWordVector.setAttributeIndices("first-last");
+        stringToWordVector.setLowerCaseTokens(true);
+        stringToWordVector.setWordsToKeep(4000);
+        
+        Instances newData = Filter.useFilter(data, numericToNominal);
+        newData = Filter.useFilter(newData, nominalToString);
+        newData = Filter.useFilter(newData, stringToWordVector);
+        
+        return newData;
+    }
     
 	public Classifier makeClassifier(Instances data) throws Exception{
 		Classifier cls = (Classifier) new NaiveBayesMultinomial();
